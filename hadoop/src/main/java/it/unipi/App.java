1efab2f8c29902985adbb25dbbc8e95d9a25a2ec
biyaG <biyagirma@gmail.com>
2026-06-19 22:05:44 +0200

    Implement EventCountMapper for GHArchive event aggregation

--- hadoop/src/main/java/it/unipi/App.java
+++ hadoop/src/main/java/it/unipi/App.java
@@ -8,6 +8,26 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(conf, "GHArchive Event Count");
+
+        job.setJarByClass(GHArchiveEventCount.class);
+
+        job.setMapperClass(EventCountMapper.class);
+        job.setCombinerClass(EventCountReducer.class);
+        job.setReducerClass(EventCountReducer.class);
+
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
     }
 }
--- hadoop/src/main/java/it/unipi/EventCountMapper.java
+++ hadoop/src/main/java/it/unipi/EventCountMapper.java
@@ -1,4 +1,39 @@
 package it.unipi;
+import java.io.IOException;
 
-public class EventCountMapper {
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.json.JSONObject;
+
+public class EventCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+        // reuse Hadoop's Writable objects
+        private final Text reducerKey = new Text();
+        private final static IntWritable one = new IntWritable(1);
+
+        @Override
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            //String line = value.toString();
+            JSONObject jsonl = new JSONObject(line);
+
+            try {
+
+
+                String eventType = jsonl.getString("type");
+                String createdAt = jsonl.getString("created_at");
+
+                JSONObject repo = jsonl.getJSONObject("repo");
+                long repoId = repo.getLong("id");
+
+                String compositeKey = repoId + "," + createdAt + "," + eventType;
+
+                reducerKey.set(compositeKey);
+                context.write(reducerKey, one);
+
+            } catch (Exception e) {
+                // Skip bad or incomplete JSON lines
+            }
+        }
 }
